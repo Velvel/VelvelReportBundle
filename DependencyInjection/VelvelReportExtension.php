50d16d592ef767cf04ba3151ec1792a039371d86
r1pp3rj4ck <attila.bukor@gmail.com>
2012-05-21 15:58:04 +0200

    [DIC] added FormBuilder to DIC

---
+++ DependencyInjection/VelvelReportExtension.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * This file is part of VelvelReportBundle (C) 2012 Velvel IT Solutions
+ *
+ * VelvelReportBundle is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation, either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * VelvelReportBundle is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General
+ * Public License along with VelvelReportBundle. If not,
+ * see <http://www.gnu.org/licenses/>.
+ */
+
+namespace Velvel\ReportBundle\DependencyInjection;
+
+use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\Config\FileLocator;
+
+class VelvelReportExtension extends Extension
+{
+    public function load(array $configs, ContainerBuilder $container)
+    {
+        $loader = new XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader->load('form.xml');
+    }
+}
